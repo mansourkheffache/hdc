b9d83cabb292458b783cd209a0b5e7375b99c1a7
Mansour Kheffache <mansourkheffache@gmail.com>
2018-02-28 15:05:34 +0200

    add support for bipolar vectors

--- hdc.py
+++ hdc.py
@@ -5,11 +5,64 @@ random.seed()
 
 class Vector:
 
-    v_size = 10000                                            # size of HDC vector
+    # deifne operations depending on representation
+
+    # binary
+    def __init_bsc(size):
+        return np.random.randint(2, size=size)
+
+    def __add_bsc(x, y):
+        z = x + y
+        z[z == 1] = np.random.randint(2, size=len(z[z == 1]))
+        z[z == 2] = np.ones(len(z[z == 2]))
+        return z
+
+    def __mul_bsc(x, y):
+        z = np.bitwise_xor(x, y)
+        return z
+
+    def __dist_bsc(x ,y):
+        z = np.bitwise_xor(x, y)
+        return (np.sum(z[z == 1]) / float(len(z)))
+
+    # biploar
+    def __init_bipolar(size):
+        return np.random.choice([-1.0, 1.0], size=size)
+
+    def __add_bipolar(x, y):
+        z = x + y
+        z[z > 1] = 1.0
+        z[z < -1] = -1.0
+        z[z == 0] = np.random.choice([-1.0, 1.0], size=len(z[z == 0]))
+        return z
+
+    def __mul_bipolar(x, y):
+        return x * y
+
+    def __dist_bipolar(x, y):
+        return (len(x) - np.dot(x, y)) / (2 * float(len(x)))
+
+    # operations list
+    __OPERATIONS = {
+        'bsc': {
+            'init': __init_bsc,
+            'add': __add_bsc,
+            'mul': __mul_bsc,
+            'dist': __dist_bsc
+        },
+        'bipolar': {
+            'init': __init_bipolar,
+            'add': __add_bipolar,
+            'mul': __mul_bipolar,
+            'dist': __dist_bipolar
+        }
+    }
 
     # init random HDC vector
-    def __init__(self):
-        self.value = np.random.randint(2, size=self.v_size)
+    def __init__(self, size, rep):
+        self.rep = rep
+        self.size = size
+        self.value = self.__OPERATIONS[rep]['init'](size)
 
     # print vector
     def __repr__(self):
@@ -21,23 +74,19 @@ class Vector:
 
     # addition
     def __add__(self, a):
-        b = Vector();
-        b.value = self.value + a.value
-        # b.value /= 2.0
-        b.value[b.value == 1] = np.random.randint(2, size=len(b.value[b.value == 1]))
-        b.value[b.value == 2] = np.ones(len(b.value[b.value == 2]))
+        b = Vector(self.size, self.rep);
+        b.value = self.__OPERATIONS[self.rep]['add'](self.value, a.value)
         return b
 
     # multiplication
     def __mul__(self, a):
-        b = Vector()
-        b.value = np.bitwise_xor(self.value, a.value)
+        b = Vector(self.size, self.rep);
+        b.value = self.__OPERATIONS[self.rep]['mul'](self.value, a.value)
         return b
 
     # distance
     def dist(self, a):
-        b = self * a
-        return (np.sum(b.value[b.value == 1]) / float(self.v_size))
+        return self.__OPERATIONS[self.rep]['dist'](self.value, a.value)
 
 
 class Space:
@@ -50,31 +99,28 @@ class Space:
     def _random_name(self):
         return ''.join(random.choice('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ') for i in range(8))
 
-    def add(self, name=None, v=None):
-        if v == None:
-            v = Vector()
-        if name == None:
-            name = self._random_name()
-
-        self.vectors[name] = v
-        return name, v
-
     def __repr__(self):
         return ''.join("'%s' , %s\n" % (v, self.vectors[v]) for v in self.vectors)
 
     def __getitem__(self, x):
-        # if isinstance(x, str):
         return self.vectors[x]
-        # elif isinstance(x, Vector):
-        #     d = 1.0
-        #     match = None
-        #     for v in self.vectors:
-        #         if self.vectors[v].dist(x) < d:
-        #             match = v
-        #             d = self.vectors[v].dist(x)
-        #     return match
-        # else:
-        #     raise ValueError
+
+    def add(self, name=None):
+        if name == None:
+            name = self._random_name()
+
+        v = Vector(self.size, self.rep)
+
+        self.vectors[name] = v
+        return v
+
+    def insert(self, v, name=None):
+        if name == None:
+            name = self._random_name()
+
+        self.vectors[name] = v
+
+        return name
 
     def find(self, x):
         d = 1.0
@@ -84,5 +130,6 @@ class Space:
             if self.vectors[v].dist(x) < d:
                 match = v
                 d = self.vectors[v].dist(x)
-        print d
-        return match
+
+        # print d
+        return match, d
